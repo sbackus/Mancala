44e0f2cfde47b91a757522c3e1b41c8068a3c5bf
sbackus <Samuel.Backus@gmail.com>
2013-03-01 17:40:27 -0500

    Sides
    a mancala board has 2 sides

--- mancala.py
+++ mancala.py
@@ -9,16 +9,16 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-class Cup:
-    def __init__(self,stones=4):
+class Cup(object):
+    def __init__(self,stones=4, next=None):
         self.stones = stones
-        self.nextCup = None
+        self.nextCup = next
 
     def getStoneCount(self):
         return self.stones
 
-    def setNext(self, cell):
-        self.nextCup = cell
+    def setNext(self, cup):
+        self.nextCup = cup
 
     def getNext(self):
         return self.nextCup
@@ -26,13 +26,49 @@ class Cup:
     def addStone(self):
         self.stones +=1
 
+class Pit(Cup):
+
     def select(self):
         self.nextCup.sew(self.stones)
         self.stones = 0
 
-    def sew(self,numberOfStones):
+    def sew(self, numberOfStones):
         if numberOfStones>0:
             self.stones +=1
         if numberOfStones-1>0:
             self.nextCup.sew(numberOfStones-1)
 
+
+class Store(Cup):
+    pass
+
+class Side(object):
+    def __init__(self,pitsPerSide=6):
+        self.cups = []
+        for i in range(0,pitsPerSide):
+            self.cups.append(Pit())
+        self.cups.append(Store())
+        for i in range(0,pitsPerSide):
+            self.cups[i].setNext(self.cups[i+1])
+
+    def getCup(self,index):
+        return self.cups[index]
+
+    def getStore(self):
+        return self.cups[-1]
+
+class Board(object):
+    def __init__(self,pitsPerSide=6):
+        self.sides = [Side(),Side()]
+        self.sides[0].getStore().setNext(self.sides[1].getCup(0))
+        self.sides[1].getStore().setNext(self.sides[0].getCup(0))
+
+    def getCup(self,side,index):
+        return self.getSide(side).getCup(index)
+
+    def getSide(self,index):
+        return self.sides[index]
+
+    def __print__(self):
+        #for cups in sides
+        print self.sides
--- testMancala.py
+++ testMancala.py
@@ -36,18 +36,18 @@ class TestCup(unittest.TestCase):
         self.assertEquals(cup.getStoneCount(), count)
 
     def test_that_when_a_cup_with_1_stone_is_selected_the_stone_is_moved_to_the_next_cup(self):
-        cup1 = Cup(1)
-        cup2 = Cup()
+        cup1 = Pit(1)
+        cup2 = Pit()
         cup1.setNext(cup2)
         cup1.select()
         self.assertEquals(cup1.getStoneCount(),0)
         self.assertEquals(cup2.getStoneCount(),5)
 
-    def test_that_when_a_cup_with_3_stones_is_selecte_they_are_distributed_correctly(self):
-        cup0 = Cup(3)
-        cup1 = Cup(1)
-        cup2 = Cup(2)
-        cup3 = Cup(3)
+    def test_that_when_a_cup_with_3_stones_is_selecte_the_stones_are_distributed_correctly(self):
+        cup0 = Pit(3)
+        cup1 = Pit(1)
+        cup2 = Pit(2)
+        cup3 = Pit(3)
         cup0.setNext(cup1)
         cup1.setNext(cup2)
         cup2.setNext(cup3)
@@ -55,6 +55,31 @@ class TestCup(unittest.TestCase):
         self.assertEquals(cup1.getStoneCount(),2)
         self.assertEquals(cup2.getStoneCount(),3)
         self.assertEquals(cup3.getStoneCount(),4)
+
+
+
+class A_New_Default_Board_Should(unittest.TestCase):
+
+    def test_be_circular_and_have_14_cups(self):
+        board = Board()
+        firstCup = board.getCup(0,0)
+        next = firstCup
+        for i in range(0,13):
+            previous = next
+            next = next.getNext()
+            self.assertNotEquals(firstCup,next)
+            self.assertNotEquals(previous,next)
+        self.assertEquals(firstCup,next.getNext())
+
+    def test_have_6_pits_and_1_store_per_side(self):
+        board = Board()
+        side0 = board.getSide(0)
+        side1 = board.getSide(1)
+        for side in[side0,side1]:
+            for i in range(0,6):
+                self.assertEquals(type(side0.getCup(i)),Pit)
+            self.assertEquals(type(side0.getCup(6)),Store)
+
 
 
 if __name__ == '__main__':
